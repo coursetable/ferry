e91ee7a08fbb89f7846893e9202e4109a876455c
Kevin Hu <kevinhuwest@gmail.com>
2019-10-14 17:01:50 -0400

    Create fetch_ratings.py

---
+++ fetch_ratings.py
@@ -0,0 +1,11 @@
+import requests
+import getpass 
+
+netid = input("Yale NetID: ")
+password = getpass.getpass()
+
+session = requests.Session()
+
+r = session.post("https://secure.its.yale.edu/cas/login?username="+netid+"&password="+password)
+
+print("Cookies: ", session.cookies.get_dict())
