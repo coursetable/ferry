c7a445bcaa77936edda5fecacb980136c76cec79
Max Yuan <maxyuan6717@gmail.com>
2019-11-04 17:23:09 -0500

    Update fetch_ratings.py
    Loops through each course of each term to find evaluation data. Still have to dump data into json

--- fetch_ratings.py
+++ fetch_ratings.py
@@ -2,42 +2,85 @@ import requests
 import getpass 
 import json
 import time
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 from includes.class_processing import *
 
+from os import listdir
+from os.path import isfile, join
+
+# Input NetID and password to login to Yale CAS
 netid = input("Yale NetID: ")
 password = getpass.getpass()
 session = requests.Session()
 
+# Login
 r = session.post("https://secure.its.yale.edu/cas/login?username="+netid+"&password="+password)
 
 print("Cookies: ", session.cookies.get_dict())
 
-url_index = "https://oce.app.yale.edu/oce-viewer/studentSummary/index"
-url_show = "https://oce.app.yale.edu/oce-viewer/studentSummary/show"
-url_graphData = "https://oce.app.yale.edu/oce-viewer/studentSummary/graphData"
+url_index = "https://oce.app.yale.edu/oce-viewer/studentSummary/index" # Main website with number of questions
+url_show = "https://oce.app.yale.edu/oce-viewer/studentSummary/show" # JSON data with question IDs
+url_graphData = "https://oce.app.yale.edu/oce-viewer/studentSummary/graphData" # JSON data with rating data for each question ID
 
-class_info = {
-    "crn": 10684,
-    "term_code": 201803
-}
+# get the list of all course JSON files as previously fetched
+term_jsons_path = "./api_output/term_courses/"
+term_jsons = [f for f in listdir(
+    term_jsons_path) if isfile(join(term_jsons_path, f))]
+term_jsons = [x for x in term_jsons if x[-5:] == ".json"]
 
-page_index = session.get(url_index,params = class_info)
-page_show = session.get(url_show)
-data_show = json.loads(page_show.text)
+# load and parse term JSONs
+for term_json_file in term_jsons:
+    term_code = term_json_file[0:6] # Get term code from file name
+    with open(term_jsons_path + term_json_file, "r") as f:
+        term_json = json.load(f)
+    
+    noEvals = 0 # Whether or not this term has evaluation data
 
-questionList = data_show['questionList']
+    for term_course in term_json: # Loop through each course in the term
+        crn_code = term_course['crn'] # Get crn code for this course
+        print("TERM:",term_code,"CRN CODE:",crn_code)
+        class_info = {
+            "crn": crn_code,
+            "term_code": term_code
+        }
 
-questionIds = []
-for question in questionList:
-    questionIds.append(question['questionId'])
+        page_index = session.get(url_index,params = class_info)
+        if page_index.status_code != 200: # Evaluation data for this term not available
+            noEvals = 1
+            break
+        
+        page_show = session.get(url_show)
+        if page_show.status_code != 200: # Evaluation data for this course not available
+            print("Evaluation data for course:",crn_code,"in term:",term_code,"is unavailable")
+            continue
+        data_show = json.loads(page_show.text)
 
-for Id in questionIds:
-    millis = int(round(time.time() * 1000))
-    question_info = {
-        "questionId": Id,
-        "_": millis
-    }
-    page_graphData = session.get(url_graphData, params = question_info)
-    data_graphData = json.loads(page_graphData.text)
-    print(data_graphData)
+        questionList = data_show['questionList']
+
+        # List of question IDs
+        questionIds = []
+        for question in questionList:
+            questionIds.append(question['questionId'])
+
+        numQuestions = len(questionIds)
+
+        if numQuestions == 0: # Evaluation data for this course not available
+            print("Evaluation data for course:",crn_code,"in term:",term_code,"is unavailable")
+            continue
+
+        for Id in questionIds:
+            millis = int(round(time.time() * 1000)) # Get current time in milliseconds
+            question_info = {
+                "questionId": Id,
+                "_": millis
+            }
+            page_graphData = session.get(url_graphData, params = question_info) # Fetch ratings data
+            data_graphData = json.loads(page_graphData.text)
+            print(data_graphData)   
+    """
+    if noEvals == 1:
+        print("NO EVALUATION DATA FOR TERM:", term_code)
+    else:
+        print("EVALUATION DATA FOR TERM:", term_code)
+    """
