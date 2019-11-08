deb65b25e784601c843c766a8052ffbd982aa517
Max Yuan <maxyuan6717@gmail.com>
2019-11-08 15:49:01 -0500

    Updated fetch_ratings.py and added json files with eval data
    Dump JSON data into new term_evals folder

--- fetch_ratings.py
+++ fetch_ratings.py
@@ -2,6 +2,7 @@ import requests
 import getpass 
 import json
 import time
+import re
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 from includes.class_processing import *
@@ -32,13 +33,19 @@ term_jsons = [x for x in term_jsons if x[-5:] == ".json"]
 # load and parse term JSONs
 for term_json_file in term_jsons:
     term_code = term_json_file[0:6] # Get term code from file name
+    output_path = "./api_output/term_evals/{}.json".format(
+            term_code)
     with open(term_jsons_path + term_json_file, "r") as f:
         term_json = json.load(f)
     
     noEvals = 0 # Whether or not this term has evaluation data
 
+    term_evals = {}
+
     for term_course in term_json: # Loop through each course in the term
         crn_code = term_course['crn'] # Get crn code for this course
+
+        term_evals[crn_code] = []
         print("TERM:",term_code,"CRN CODE:",crn_code)
         class_info = {
             "crn": crn_code,
@@ -47,6 +54,7 @@ for term_json_file in term_jsons:
 
         page_index = session.get(url_index,params = class_info)
         if page_index.status_code != 200: # Evaluation data for this term not available
+            print("Evaluation data for term:",term_code,"is unavailable")
             noEvals = 1
             break
         
@@ -77,7 +85,21 @@ for term_json_file in term_jsons:
             }
             page_graphData = session.get(url_graphData, params = question_info) # Fetch ratings data
             data_graphData = json.loads(page_graphData.text)
-            print(data_graphData)   
+
+            #print(data_graphData)
+            evals_data = []
+            for x in range(len(data_graphData[0]['data'])):
+                evals_data.append(data_graphData[0]['data'][x][1])
+            term_evals[crn_code].append(evals_data)
+
+        print(term_evals[crn_code])
+    if (noEvals == 1):
+        continue
+    output = json.dumps(term_evals, indent=4)
+    with open(output_path, "w") as f:
+        #f.write(json.dumps(term_evals, indent=4))
+        f.write(re.sub(r'",\s+', '", ', output))
+
     """
     if noEvals == 1:
         print("NO EVALUATION DATA FOR TERM:", term_code)
