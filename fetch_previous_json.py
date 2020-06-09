42b6c88ffaa4f14560cc7b6b0bc2ce3dc1df7730
Kevin Hu <kevinhuwest@gmail.com>
2020-06-09 18:18:24 -0400

    Updated previous JSON fetcher

--- fetch_previous_json.py
+++ fetch_previous_json.py
@@ -17,25 +17,31 @@ CourseTable website and outputs the following into
 
 # define list of seasons
 
-middle_years = [str(x) for x in range(2010,2020)]
+middle_years = [str(x) for x in range(2010, 2020)]
 
 spring_seasons = [str(x) + "01" for x in middle_years]
 summer_seasons = [str(x) + "02" for x in middle_years]
 winter_seasons = [str(x) + "03" for x in middle_years]
 
-seasons = ["200903"] + spring_seasons + summer_seasons + winter_seasons + ["202001"]
+seasons = [
+    "200903",
+    *spring_seasons,
+    *summer_seasons,
+    *winter_seasons,
+    "202001", "202002"
+]
 
 # get lists of classes per season
 for season in seasons:
 
     # evaluations included
-    print("Fetching previous JSON for season {} (with evals)".format(season))
+    print(f"Fetching previous JSON for season {season} (with evals)")
     previous_json = fetch_previous_json(season, evals=True)
-    with open("./api_output/previous_json/evals_"+season+".json", "w") as f:
+    with open(f"./api_output/previous_json/evals_{season}.json", "w") as f:
         f.write(json.dumps(previous_json, indent=4))
 
     # evaluations excluded
-    print("Fetching previous JSON for season {} (without evals)".format(season))
+    print(f"Fetching previous JSON for season {season} (without evals)")
     previous_json = fetch_previous_json(season, evals=False)
-    with open("./api_output/previous_json/"+season+".json", "w") as f:
+    with open(f"./api_output/previous_json/{season}.json", "w") as f:
         f.write(json.dumps(previous_json, indent=4))
