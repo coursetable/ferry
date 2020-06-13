acf3ee22d6aae38424ad83dfe06e9099f3c082c4
Harshal Sheth <hsheth2@gmail.com>
2020-06-12 23:19:16 -0400

    compute historical ratings

--- ferry/computed.py
+++ ferry/computed.py
@@ -169,8 +169,63 @@ def course_invariants(session):
 
 
 def historial_ratings_computed(session):
-    # TODO
-    pass
+    """
+    Update: historical_ratings (create entries as needed)
+    """
+    query = (
+        session.query(database.Listing.course_code, database.Course)
+        .join(database.Course)
+        .options(sqlalchemy.orm.joinedload(database.Course.professors))
+    )
+
+    for course_code, course in query:
+        for professor in course.professors:
+            historical_ratings, _ = database.get_or_create(
+                session,
+                database.HistoricalRating,
+                course_code=course_code,
+                professor_id=professor.professor_id,
+            )
+
+            # Course rating - all professors.
+            rating_all = (
+                session.query(
+                    sqlalchemy.func.avg(database.EvaluationStatistics.avg_rating)
+                )
+                .select_from(database.Listing)
+                .join(database.Course)
+                .join(database.EvaluationStatistics)
+                .filter(database.Listing.course_code == course_code)
+            )
+            historical_ratings.course_rating_all_profs = rating_all.scalar()
+
+            # Course rating - this professor.
+            rating_this = (
+                session.query(
+                    sqlalchemy.func.avg(database.EvaluationStatistics.avg_rating)
+                )
+                .select_from(database.Listing)
+                .join(database.Course)
+                .join(database.EvaluationStatistics)
+                .join(database.course_professors)
+                .filter(database.Listing.course_code == course_code)
+                .filter(
+                    database.course_professors.c.professor_id == professor.professor_id
+                )
+            )
+            historical_ratings.course_rating_this_prof = rating_this.scalar()
+
+            # Course workload.
+            workload_all = (
+                session.query(
+                    sqlalchemy.func.avg(database.EvaluationStatistics.avg_workload)
+                )
+                .select_from(database.Listing)
+                .join(database.Course)
+                .join(database.EvaluationStatistics)
+                .filter(database.Listing.course_code == course_code)
+            )
+            historical_ratings.course_workload = workload_all.scalar()
 
 
 def professors_computed(session):
@@ -203,8 +258,8 @@ if __name__ == "__main__":
         questions_computed,
         question_tag_invariant,
         evaluation_statistics_computed,
-        historial_ratings_computed,
         professors_computed,
+        historial_ratings_computed,
     ]
 
     for fn in items:
@@ -215,11 +270,3 @@ if __name__ == "__main__":
 
         with database.session_scope(database.Session) as session:
             fn(session)
-
-
-"""
-TODO: computed fields
-  214:         comment="[computed] The average rating for this course code, across all professors who taught it",
-  218:         comment="[computed] The average rating for this course code when taught by this professor",
-  222:         comment="[computed] The average workload for this course code, across all times it was taught",
-"""
