3ac5b882470d480492a23343b294a33759b7ddc2
Harshal Sheth <hsheth2@gmail.com>
2020-06-16 20:13:15 -0400

    Progress on upload

--- drive_fetch.sh
+++ drive_fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Pulling data from storage'
-rclone copy coursetable:ferry-data/drive_cache.tar.gz .
+rclone copy coursetable:ferry-data/drive_cache.tar.gz . --progress
 
 echo 'Extracting from archive'
 tar -xvf drive_cache.tar.gz
--- drive_push.sh
+++ drive_push.sh
@@ -4,4 +4,4 @@ echo 'Generating compressed data bundle'
 tar -czf drive_cache.tar.gz api_output
 
 echo 'Uploading to storage'
-rclone copy drive_cache.tar.gz coursetable:ferry-data
+rclone copy drive_cache.tar.gz coursetable:ferry-data --progress
