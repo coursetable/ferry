fc52890f092fd74a9cdace032cade221865e289f
Harshal Sheth <hsheth2@gmail.com>
2020-06-30 22:28:51 -0400

    Print table row counts

--- ferry/computed.py
+++ ferry/computed.py
@@ -337,3 +337,23 @@ if __name__ == "__main__":
 
         with database.session_scope(database.Session) as session:
             fn(session)
+
+    # Print row counts for each table.
+    tqdm.write("\nTable Statistics")
+    with database.session_scope(database.Session) as session:
+        # Via https://stackoverflow.com/a/2611745/5004662.
+        sql = """
+        select table_schema,
+            table_name,
+            (xpath('/row/cnt/text()', xml_count))[1]::text::int as row_count
+        from (
+        select table_name, table_schema,
+                query_to_xml(format('select count(*) as cnt from %I.%I', table_schema, table_name), false, true, '') as xml_count
+        from information_schema.tables
+        where table_schema = 'public' --<< change here for the schema you want
+        ) t
+        """
+
+        result = session.execute(sql)
+        for table_counts in result:
+            print(f"{table_counts[1]:>25} - {table_counts[2]:6} rows")
