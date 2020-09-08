5428fa0d7b067a7ffe86eb7f72ed67d6f4e246fe
Harshal Sheth <hsheth2@gmail.com>
2020-09-07 23:03:53 -0400

    Update cron script

--- refresh_courses.sh
+++ refresh_courses.sh
@@ -26,17 +26,14 @@ poetry run python ./ferry/crawler/fetch_classes.py -s LATEST_3
 announce "Parsing all classes"
 poetry run python ./ferry/crawler/parse_classes.py
 
-# announce "Fetching ratings for latest year"
-# poetry run python ./ferry/crawler/fetch_ratings.py -s LATEST_3
-
 # announce "Fetching demand statistics for latest year"
 # poetry run python ./ferry/crawler/fetch_demand.py -s LATEST_3
 
 announce "Creating and uploading Google Drive backup"
 sh ./drive_push.sh
 
-announce "Importing to database"
-poetry run python ./ferry/importer.py
+announce "Importing courses to database"
+poetry run python ./ferry/importer.py --mode courses
 
 announce "Generating computed database fields"
 poetry run python ./ferry/computed.py
