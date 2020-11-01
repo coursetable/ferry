a4ab7a7baaf3a20d81794db7d6b414fe20b3e154
Kevin Hu <kevinhuwest@gmail.com>
2020-11-01 18:44:39 -0500

    Parse registrar's notes

--- ferry/includes/class_processing.py
+++ ferry/includes/class_processing.py
@@ -961,6 +961,7 @@ def extract_course_info(course_json, season, fysem):
     course_info["areas"] = found_items(course_json["yc_attrs"], areas_map)
 
     course_info["flags"] = extract_flags(course_json["ci_attrs"])
+    course_info["regnotes"] = course_json["ci_attrs"]
 
     # Course homepage
     matched_homepage = re.findall(
