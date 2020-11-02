9cc07570edd8a1a32d68e3198a305b67157dfa1f
Kevin Hu <kevinhuwest@gmail.com>
2020-11-01 19:03:12 -0500

    Add registrar's notes to db

--- ferry/includes/importer.py
+++ ferry/includes/importer.py
@@ -98,6 +98,7 @@ def import_courses(merged_course_info, seasons: List[str]):
     listings["course_id"] = listings["temp_course_id"].apply(temp_to_course_id.get)
     listings["section"] = listings["section"].apply(lambda x: x if x == x else "0")
     listings["section"] = listings["section"].fillna("0").astype(str)
+    listings["section"] = listings["section"].replace({"": "0"})
 
     print("Creating professors table")
     # initialize professors table
