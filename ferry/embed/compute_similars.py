6ec88914db86d7f60148aaad46e57ae245b001db
Kevin Hu <kevinhuwest@gmail.com>
2020-11-11 22:41:03 -0500

    Clean up compute_similars.py

--- ferry/embed/compute_similars.py
+++ ferry/embed/compute_similars.py
@@ -1,12 +1,19 @@
-# pylint: skip-file
-import numpy as np
-import pandas as pd
-import ujson
+"""
+Computes similar courses per season via maximum-inner product search for both FastText
+and TF-IDF embeddings. Intended to be run immediately after /ferry/transform.py so
+foreign keys match.
 
-from ferry import config, database
+Outputs the fasttext_similars and tfidf_similars tables into /data/importer_dumps ready
+for input into /ferry/stage.py.
+"""
+import pandas as pd
+
+from ferry import config
 from ferry.includes.similarity_search import get_nearest_neighbors
 
 MAX_NEAREST_NEIGHBORS = 16
+
+print("Loading courses and embeddings")
 
 courses = pd.read_csv(config.DATA_DIR / "importer_dumps/courses.csv", index_col=0)
 
@@ -23,10 +30,11 @@ tfidf_embeddings = pd.read_hdf(
     key="embeddings",
 )
 
-# convert to Numpy arrays
+# convert to NumPy arrays
 fasttext_embeddings = fasttext_embeddings.values
 tfidf_embeddings = tfidf_embeddings.values
 
+# map all courses to embedding indices
 course_to_embed_index = dict(
     zip(unique_courses["title"], unique_courses["embed_index"])
 )
@@ -34,6 +42,7 @@ courses["embed_index"] = courses["title"].apply(course_to_embed_index.get)
 courses.dropna(subset=["embed_index"], inplace=True)
 courses["embed_index"] = courses["embed_index"].astype(int)
 
+# group embedding indices
 courses_by_season = courses.set_index("course_id").groupby("season_code")["embed_index"]
 
 seasons = courses_by_season.groups.keys()
@@ -41,18 +50,24 @@ seasons = courses_by_season.groups.keys()
 fasttext_similars = dict()
 tfidf_similars = dict()
 
+print("Computing similar courses by season")
 for season in seasons:
 
     print(f"Computing similar courses for season {season}")
 
     season_courses = courses_by_season.get_group(season)
 
+    # course_ids for database foreign keys
     season_course_ids = season_courses.index.values
+
+    # indices of matched embeddings
     season_embed_indices = season_courses.values
 
+    # embedding vectors for both methods
     season_fasttext = fasttext_embeddings[season_embed_indices]
     season_tfidf = tfidf_embeddings[season_embed_indices]
 
+    # get similar courses
     season_fasttext_similars = get_nearest_neighbors(
         season_course_ids, season_fasttext, MAX_NEAREST_NEIGHBORS
     )
@@ -60,9 +75,12 @@ for season in seasons:
         season_course_ids, season_tfidf, MAX_NEAREST_NEIGHBORS
     )
 
+    # update dictionary for season
     fasttext_similars.update(season_fasttext_similars)
     tfidf_similars.update(season_tfidf_similars)
 
+print("Aggregating similar courses")
+# convert dictionary result to DataFrames
 fasttext_similars = pd.Series(fasttext_similars).apply(list).explode()
 tfidf_similars = pd.Series(tfidf_similars).apply(list).explode()
 
@@ -73,14 +91,18 @@ tfidf_similars = pd.DataFrame.from_dict(
     dict(zip(tfidf_similars.index, tfidf_similars.values))
 )
 
+# take transpose to make each edge a row
 fasttext_similars = fasttext_similars.T
 tfidf_similars = tfidf_similars.T
 
+# reset the courses index to its own column
 fasttext_similars.reset_index(drop=False, inplace=True)
 tfidf_similars.reset_index(drop=False, inplace=True)
 
+# specify column names for database compatibility
 fasttext_similars.columns = ["source", "target", "rank"]
 tfidf_similars.columns = ["source", "target", "rank"]
 
+print("Writing output tables")
 fasttext_similars.to_csv(config.DATA_DIR / "importer_dumps/fasttext_similars.csv")
 tfidf_similars.to_csv(config.DATA_DIR / "importer_dumps/tfidf_similars.csv")
