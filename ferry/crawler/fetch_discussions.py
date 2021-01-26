f8102c775d2779507467bbc3e5e8a26311e878e1
kevinhu <kevinhuwest@gmail.com>
2021-01-25 23:51:53 -0500

    Specify season when fetching discussions

--- ferry/crawler/fetch_discussions.py
+++ ferry/crawler/fetch_discussions.py
@@ -1,13 +1,43 @@
+"""
+Fetch discussion sections.
+
+Pulls the discussion sections and locations PDF and parses to a CSV.
+"""
+import click
 import tabula
-import pandas as pd
 
 from ferry.config import DATA_DIR
 
-discussions = tabula.read_pdf(
-    "https://www.sis.yale.edu/buildings/Discussion_locations.pdf",
-    pandas_options={"names":["section_crn","subject","number","section","info","time"]},
-    multiple_tables=False,
-    pages="all",
-)[0]
 
-discussions.to_csv(DATA_DIR / "discussion_sections" / "raw_sections.csv",index=False)
+@click.command()
+@click.option(
+    "-s",
+    "--season",
+    required=True,
+    type=str,
+    help="Season current discussion sections are under. Used for naming output file.",
+)
+def fetch_discussions(season: str):
+    """
+    Fetch discussion sections in a season.
+
+    The season is not used to determine the source of the discussion sections, but rather
+    is the season for which the current discussion sections posted at
+    "https://www.sis.yale.edu/buildings/Discussion_locations.pdf" belong to.
+    """
+    discussions = tabula.read_pdf(
+        "https://www.sis.yale.edu/buildings/Discussion_locations.pdf",
+        pandas_options={
+            "names": ["section_crn", "subject", "number", "section", "info", "time"]
+        },
+        multiple_tables=False,
+        pages="all",
+    )[0]
+
+    discussions.to_csv(
+        DATA_DIR / "discussion_sections" / "raw_csvs" / f"{season}.csv", index=False
+    )
+
+
+if __name__ == "__main__":
+    fetch_discussions()  # pylint: disable=no-value-for-parameter
