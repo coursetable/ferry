30c924ddcaa1a187f8e910d38a66518b2674235e
Kevin Hu <kevinhuwest@gmail.com>
2021-11-12 17:08:02 -0500

    Stringify professor_ids in computed

--- resources/computed_listing_info_tmp.sql
+++ resources/computed_listing_info_tmp.sql
@@ -30,7 +30,7 @@ WITH listing_info
                                   JOIN professors p on course_professors.professor_id = p.professor_id
                          WHERE course_professors.course_id = courses.course_id
                          GROUP BY course_professors.course_id), '[]'::jsonb) AS professor_names,
-                coalesce((SELECT jsonb_agg(p.professor_id)
+                coalesce((SELECT jsonb_agg(p.professor_id::varchar(8))
                          FROM course_professors
                                   JOIN professors p on course_professors.professor_id = p.professor_id
                          WHERE course_professors.course_id = courses.course_id
