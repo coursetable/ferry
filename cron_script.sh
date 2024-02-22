bfa87cd6fba0ea8c8f277bbd6cb6eca7ceddcd6d
Neil Song <song.neil.song@gmail.com>
2024-02-22 01:31:43 -0500

    add cron_script

---
+++ cron_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+PING_URL="https://hc-ping.com/fd248442-b2ea-457c-9c52-a087eaaf80bd"
+
+curl -fsS -m 10 --retry 5 -o /dev/null "${PING_URL}/start"
+
+export TERM=dumb
+source .venv/bin/activate
+python main.py -f config/release_courses.yml
+
+curl -fsS -m 10 --retry 5 -o /dev/null "${PING_URL}$([ $? -ne 0 ] && echo -n /fail)"
