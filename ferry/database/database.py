008382aa087dd795c189112268ede39add83d595
Neil Song <song.neil.song@gmail.com>
2024-02-24 02:46:12 -0500

    more stable db connection

--- ferry/database/database.py
+++ ferry/database/database.py
@@ -21,7 +21,20 @@ def create_engine_and_session(connect_string: str):
     -------
     Engine and Session
     """
-    Engine = sqlalchemy.create_engine(connect_string)
+    Engine = sqlalchemy.create_engine(
+        connect_string,
+        pool_size=10,
+        max_overflow=2,
+        pool_recycle=300,
+        pool_pre_ping=True,
+        pool_use_lifo=True,
+        connect_args={
+            "keepalives": 1,
+            "keepalives_idle": 30,
+            "keepalives_interval": 10,
+            "keepalives_count": 5,
+        }
+    )
     Base.metadata.create_all(Engine)
 
     if any(not table.endswith("_staged") for table in Base.metadata.tables):
