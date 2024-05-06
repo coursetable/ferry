de639d7bfd74d2289836275efcf0de9844a0ffb9
Joshua Chen <sidachen2003@gmail.com>
2024-05-05 20:53:26 -0400

    fix: DB initialization

--- ferry/database/__init__.py
+++ ferry/database/__init__.py
@@ -1,6 +1,9 @@
-import sqlalchemy
-from contextlib import contextmanager
-
+from .database import (
+    Database,
+    InvariantError,
+    MissingTablesError,
+    session_scope,
+)
 from .models import (
     Base,
     Course,
@@ -17,61 +20,3 @@ from .models import (
 )
 from .stage import stage
 from .deploy import deploy
-
-
-class Database:
-    def __init__(self, connect_string: str):
-        super().__init__()
-        self.connect_string = connect_string
-        self.Engine = sqlalchemy.create_engine(
-            connect_string,
-            pool_size=10,
-            max_overflow=2,
-            pool_recycle=300,
-            pool_pre_ping=True,
-            pool_use_lifo=True,
-            connect_args={
-                "keepalives": 1,
-                "keepalives_idle": 30,
-                "keepalives_interval": 10,
-                "keepalives_count": 5,
-            },
-        )
-        Base.metadata.create_all(self.Engine)
-
-        if any(not table.endswith("_staged") for table in Base.metadata.tables):
-            raise MissingTablesError("Model tables should all end with _staged")
-
-        self.Session = sqlalchemy.orm.sessionmaker(bind=self.Engine)
-
-
-class InvariantError(Exception):
-    """
-    Object for invariant checking exceptions.
-    """
-
-    # pylint: disable=unnecessary-pass
-    pass
-
-
-class MissingTablesError(Exception):
-    """
-    Object for missing table exceptions.
-    """
-
-    # pylint: disable=unnecessary-pass
-    pass
-
-
-@contextmanager
-def session_scope(session_context, *args, **kwargs):
-    """Provide a transactional scope around a series of operations."""
-    session = session_context(*args, **kwargs)
-    try:
-        yield session
-        session.commit()
-    except:
-        session.rollback()
-        raise
-    finally:
-        session.close()
